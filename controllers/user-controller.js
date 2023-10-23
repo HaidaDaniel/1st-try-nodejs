1a4b0cb8deee0561a82f560ccc3f0ee9892631e0
Daniel Haida <danilgajda22@gmail.com>
2023-10-23 13:56:07 +0300

    login logout refresh

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,29 +1,46 @@
 const userService = require('../services/user-service')
-const UserService = require('../services/user-service')
+const { validationResult } = require('express-validator')
+const ApiError = require('../exceptions/api-error')
 
 class UserController {
   async registration(req, res, next) {
     try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequest('validation error', errors.array()))
+      }
       const { email, password } = req.body
       const userData = await userService.registration(email, password)
       res.cookie('refreshToken', userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true
       })
-
       return res.json(userData)
     } catch (e) {
       next(e)
     }
   }
+
   async login(req, res, next) {
     try {
+      const { email, password } = req.body
+      const userData = await userService.login(email, password)
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true
+      })
+      return res.json(userData)
     } catch (e) {
       next(e)
     }
   }
+
   async logout(req, res, next) {
     try {
+      const { refreshToken } = req.cookies
+      const token = await userService.logout(refreshToken)
+      res.clearCookie('refreshToken')
+      return res.json(token)
     } catch (e) {
       next(e)
     }
@@ -39,13 +56,22 @@ class UserController {
   }
   async refresh(req, res, next) {
     try {
+      const { refreshToken } = req.cookies
+      const userData = await userService.refresh(refreshToken)
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true
+      })
+      return res.json(userData)
     } catch (e) {
       next(e)
     }
   }
+
   async getUsers(req, res, next) {
     try {
-      res.json(['weqawr', 'safdgfdg', '11111'])
+      const users = await userService.getAllUsers()
+      return res.json(users)
     } catch (e) {
       next(e)
     }
--- services/user-service.js
+++ services/user-service.js
@@ -2,7 +2,6 @@ const UserModel = require('../models/user-model')
 const bcrypt = require('bcrypt')
 const uuid = require('uuid')
 const mailService = require('./mail-service')
-const TokenService = require('./token-service')
 const tokenService = require('./token-service')
 const UserDto = require('../dtos/user-dto')
 const ApiError = require('../exceptions/api-error')
@@ -42,5 +41,45 @@ class UserService {
     user.isActivated = true
     await user.save()
   }
+  async login(email, password) {
+    const user = await UserModel.findOne({ email })
+    if (!user) {
+      throw ApiError.BadRequest('user with this email not found')
+    }
+    const isPassEquals = await bcrypt.compare(password, user.password)
+    if (!isPassEquals) {
+      throw ApiError.BadRequest('wrong password')
+    }
+    const userDto = new UserDto(user)
+    const tokens = tokenService.generateTokens({ ...userDto })
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken)
+    return { ...tokens, user: userDto }
+  }
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken)
+    return token
+  }
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.UnauthorizedError()
+    }
+    const userData = tokenService.validateRefreshToken(refreshToken)
+    const tokenFromDb = await tokenService.findToken(refreshToken)
+    if (!userData || !tokenFromDb) {
+      throw ApiError.UnauthorizedError()
+    }
+    const user = await UserModel.findById(userData.id)
+    const userDto = new UserDto(user)
+    const tokens = tokenService.generateTokens({ ...userDto })
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken)
+    return { ...tokens, user: userDto }
+  }
+  async getAllUsers() {
+    const users = await UserModel.find()
+    return users
+  }
 }
+
 module.exports = new UserService()
